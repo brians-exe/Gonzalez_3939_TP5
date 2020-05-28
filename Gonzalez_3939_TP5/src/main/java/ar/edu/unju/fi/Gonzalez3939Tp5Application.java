b04e6ae0e2da67aa434542c175a75431a08f6abe
Brian's <Brian's@Brians-PC>
2020-05-28 16:19:14 -0300

    Asunto: Inicialización del proyecto. Responsable: Gonzalez,Brian L. Task00: Inicialización del proyecto.

---
+++ Gonzalez_3939_TP5/src/main/java/ar/edu/unju/fi/Gonzalez3939Tp5Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gonzalez3939Tp5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gonzalez3939Tp5Application.class, args);
+	}
+
+}
